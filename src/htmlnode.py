ef7e70e50682a8e9566c2c45ccb9f7babd3f700a
jeremi_nguyen <jereminguyen123@gmail.com>
2026-05-19 00:30:28 -0700

    Added child class of HTMLNode called LeafNode that will represents a single HTML tag with no children

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,20 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str | None, props=None):
+        super().__init__(tag, value, None, props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLnode(unittest.TestCase):
     def test_eq_to_html(self):
@@ -34,3 +34,20 @@ class TestHTMLnode(unittest.TestCase):
             node.props_to_html(),
             ' href="https://www.google.com"'
         )
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_props_leaf_to_html_p(self):
+        node = LeafNode("a", "This is a paragraph of text.", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">This is a paragraph of text.</a>')
+
+    def test_none_tag_leaf_to_html_p(self):
+        node = LeafNode(None, "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "This is a paragraph of text.")
+
+    def test_none_value_leaf_to_html_p(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
