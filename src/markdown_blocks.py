b8127ee3b869c77a9b5ba6f5a51145b592bca9e9
jeremi_nguyen <jereminguyen123@gmail.com>
2026-05-24 20:55:53 -0700

    Created block_to_block_type function in markdown_blocks.py in order to determine what type a block is using the enum values I have also created, also made some unit test for the function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,14 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     split_markdown = markdown.split("\n\n")
     result = []
@@ -7,3 +18,26 @@ def markdown_to_blocks(markdown):
            continue
        result.append(new_line)
     return result
+    
+def block_to_block_type(markdown_block):
+    if re.match(r"^#{1,6} .+", markdown_block):
+        return BlockType.HEADING
+
+    if markdown_block.startswith("```\n") and markdown_block.endswith("```"):
+        return BlockType.CODE
+
+    lines = markdown_block.split("\n")
+
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+
+    if all(re.match(r"^- .+", line) for line in lines):
+        return BlockType.UNORDERED_LIST
+
+    if all(
+        re.match(rf"^{i}\. .+", line)
+        for i, line in enumerate(lines, start=1)
+    ):
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -21,3 +21,39 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_paragraph_block_to_block_type(self):
+        block = "This is a paragraph of text. It has some **bold** and _italic_ words inside of it."
+        expected = BlockType.PARAGRAPH
+        result = block_to_block_type(block)
+        self.assertEqual(expected, result)
+    
+    def test_heading_block_to_block_type(self):
+        block = "###### This is a heading"
+        expected = BlockType.HEADING
+        result = block_to_block_type(block)
+        self.assertEqual(expected, result)
+
+    def test_code_block_to_block_type(self):
+        block = "```\nThis is a code type\n```"
+        expected = BlockType.CODE
+        result = block_to_block_type(block)
+        self.assertEqual(expected, result)
+
+    def test_quote_block_to_block_type(self):
+        block = """> This is a quote > This is still part of the same quote > Another quoted line"""
+        expected = BlockType.QUOTE
+        result = block_to_block_type(block)
+        self.assertEqual(expected, result)
+        
+    def test_unordered_list_block_to_block_type(self):
+        block = """- This is a unordered_list - This is still part of the same unordered_list - Another unordered_list line"""
+        expected = BlockType.UNORDERED_LIST
+        result = block_to_block_type(block)
+        self.assertEqual(expected, result)
+    
+    def test_ordered_list_block_to_block_type(self):
+        block = """1. This is a ordered_list 2. This is still part of the same ordered_list 3. Another ordered_list line"""
+        expected = BlockType.ORDERED_LIST
+        result = block_to_block_type(block)
+        self.assertEqual(expected, result)
