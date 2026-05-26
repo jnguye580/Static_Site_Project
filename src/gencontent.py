6dc2436d079daea48f0b320c41fea784b143d9bb
jeremi_nguyen <jereminguyen123@gmail.com>
2026-05-25 19:32:05 -0700

    Add generate_page and serve site locally
    - Implement generate_page to render markdown through a template
    - Wire up main.py to generate public/index.html from content/index.md
    - Update main.sh to start a local http server on port 8888

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
-from markdown_blocks import BlockType
+import os
+from markdown_blocks import BlockType, markdown_to_html_node
 
 def extract_title(markdown):
     markdown_lines = markdown.split("\n")
@@ -8,3 +9,29 @@ def extract_title(markdown):
             return new_line.strip()
     raise Exception("no h1 header found")
     
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        markdown_content = f.read()
+    with open(template_path) as t:
+        template_content = t.read()
+    title = extract_title(markdown_content)
+    html_node = markdown_to_html_node(markdown_content)
+    html_string = html_node.to_html()
+
+    temp = template_content.replace("{{ Title }}", title)
+    final = temp.replace("{{ Content }}", html_string)
+
+    dir_path = os.path.dirname(dest_path)
+
+    if dir_path:
+        os.makedirs(dir_path, exist_ok=True)
+
+    with open(dest_path, "w") as f:
+        f.write(final)
+
+   
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from gencontent import generate_page
 from textnode import TextNode, TextType
 from copystatic import copy_static
 
 def main():
     copy_static("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
     
 if __name__ == "__main__":
